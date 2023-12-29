07f8f291a207c4fb3b5658eda35ee4afa098585c
techmccat <alessioa02004@gmail.com>
2023-12-29 21:00:57 +0100

    Update spi-hal-one example/benchmark

--- examples/spi-hal-one.rs
+++ examples/spi-hal-one.rs
@@ -1,19 +1,19 @@
 // This example is to test the SPI without any external devices.
 // It puts "Hello world!" on the mosi-line and logs whatever is received on the miso-line to the info level.
 // The idea is that you should connect miso and mosi, so you will also receive "Hello world!".
+// It also transmits the data again using the old implementation, so those with a logic analyzer
+// can appreciate the effects of prefilling the TX FIFO
 
 #![no_main]
 #![no_std]
 
 use crate::hal::{
-    delay::DelayFromCountDownTimer,
     prelude::*,
     pwr::PwrExt,
     rcc::Config,
     spi,
     stm32::Peripherals,
-    time::{ExtU32, RateExtU32},
-    timer::Timer,
+    time::RateExtU32,
 };
 
 use cortex_m_rt::entry;
@@ -32,9 +32,10 @@ fn main() -> ! {
     let dp = Peripherals::take().unwrap();
     let rcc = dp.RCC.constrain();
     let pwr = dp.PWR.constrain().freeze();
-    let mut rcc = rcc.freeze(Config::hsi(), pwr);
-    let timer2 = Timer::new(dp.TIM2, &rcc.clocks);
-    let mut delay_tim2 = DelayFromCountDownTimer::new(timer2.start_count_down(100.millis()));
+    let mut rcc = rcc.freeze(
+        Config::hsi(),
+        pwr
+    );
 
     // let gpioa = dp.GPIOA.split(&mut rcc);
     let gpioa = dp.GPIOA.split(&mut rcc);
@@ -42,9 +43,11 @@ fn main() -> ! {
     let miso = gpioa.pa6.into_alternate();
     let mosi = gpioa.pa7.into_alternate();
 
+    // 1/8 SPI/SysClk ratio seems to be the upper limit for continuous transmission one bit at a
+    // time
     let mut spi = dp
         .SPI1
-        .spi((sclk, miso, mosi), spi::MODE_0, 400.kHz(), &mut rcc);
+        .spi((sclk, miso, mosi), spi::MODE_0, 2.MHz(), &mut rcc);
     let mut cs = gpioa.pa8.into_push_pull_output();
     cs.set_high().unwrap();
 
@@ -57,14 +60,11 @@ fn main() -> ! {
     spi.flush().unwrap();
     cs.set_high().unwrap();
 
-    info!("Received {:?}", core::str::from_utf8(received).ok());
-    delay_tim2.delay_ms(10_u16);
-
     cs.set_low().unwrap();
-    embedded_hal::blocking::spi::Write::write(&mut spi, received).unwrap();
+    embedded_hal::blocking::spi::Write::write(&mut spi, MESSAGE).unwrap();
     cs.set_high().unwrap();
+    info!("Received {:?}", core::str::from_utf8(received).ok());
 
-    // info!("{:?}", core::str::from_utf8(received).ok());
     loop {
         cortex_m::asm::nop();
     }
